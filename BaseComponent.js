5b9b7d0e9d7b2d05f80b50f4c5fcaa3352ae70ca
matthewstyers <mattysty@gmail.com>
2016-10-26 22:56:12 -0500

    lint

--- BaseComponent.js
+++ BaseComponent.js
@@ -1,9 +1,7 @@
-'use strict';
-
 import React from 'react';
 
 export default class BaseComponent extends React.Component {
-    _bind(...methods) {
-        methods.forEach( (method) => this[method] = this[method].bind(this) );
-    }
+  _bind(...methods) {
+    methods.forEach( (method) => this[method] = this[method].bind(this) );
+  }
 }
